2c0cf2e8823e6fd4f0806cda6cf2cacae2235686
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-03-05 21:54:01 +0900

    feat: implemented GET todos and GET todos/1

--- todo/backend/config/settings.py
+++ todo/backend/config/settings.py
@@ -33,7 +33,11 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Local
     'todos',
-    
+
+    # 3rd parth
+    'rest_framework',
+    'corsheaders',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
