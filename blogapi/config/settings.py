edd3083f81b2a9f6f20aac55f2f42a30b8632116
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-03-06 21:02:48 +0900

    feat: created CRUD for posts

--- blogapi/config/settings.py
+++ blogapi/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+    'rest_framework',
+
+    # Local
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
